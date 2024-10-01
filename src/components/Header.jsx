da3fefaa6bf377eb940172522c3fc93dca0e0915
GeorgessDavid <georgessdavid@gmail.com>
2024-10-01 15:52:33 -0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#aboutMe">Sobre Mí</a></li>
+                    <li><a href="#tecnologies">Tecnologías</a></li>
+                    <li><a href="#projects">Proyectos</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+export const Main = () => {
+    return (
+        <main>
+            <section className="main-section">
+                <h1 className="main-title">Full-Stack <br/> Web Developer</h1>
+            </section>
+            <section className="main-tecnologies">
+                <img src="/public/img/node.png" alt="node.png" title="Node.JS"/>
+                <img src="/public/img/express.png" alt="express.png" title="Express.JS"/>
+                <img src="/public/img/react.png" alt="react.png" title="React.JS"/>
+                <img src="/public/img/mysql.png" alt="mysql.png" title="MySQL"/>
+                <img src="/public/img/sequelize.png" alt="sequelize.png" title="Sequelize.JS"/>
+                <img src="/public/img/python.png" alt="python.png" title="Python"/>
+                <img src="/public/img/jwt.png" alt="jwt.png" title="JSON Web Token"/>
+                <img src="/public/img/html.png" alt="html.png" title="HTML5"/>
+                <img src="/public/img/css.png" alt="css.png" title="CSS3"/>
+                <img src="/public/img/javascript.png" alt="javascript.png" title="JavaScript"/>
+            </section>
+        </main>
+    )
+}
