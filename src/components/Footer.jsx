f8d674094f9efa3ce165e51f0cbf8a3bdd90b63e
GeorgessDavid <georgessdavid@gmail.com>
2025-06-02 21:51:51 -0300

    cambios en varios archivos.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,33 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export const Footer = () => {
-    return (
-        <footer>
-            <a href="https://github.com/GeorgessDavid" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} size="2xl" title="GitHub" /></a>
-            <a href="https://www.linkedin.com/in/georges-david/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2xl" title="LinkedIn" /></a>
-            <a href="mailto:georgesdavid.dev@gmail.com" rel="noreferrer"><FontAwesomeIcon icon={faEnvelope} size="2xl" title="Gmail" /></a>
-        </footer>
-    )
-}
+  return (
+    <footer className="flex justify-center items-center bg-[#212529] text-[#F83D3D] py-4 px-8 border-t-2 border-[#F83D3D] gap-4">
+      <a
+        href="https://github.com/GeorgessDavid"
+        target="_blank"
+        rel="noreferrer"
+        className="text-[#F83D3D] hover:text-white transition-all duration-300 ease-in-out"
+      >
+        <FontAwesomeIcon icon={faGithub} size="2xl" title="GitHub" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/georges-david/"
+        target="_blank"
+        rel="noreferrer"
+        className="text-[#F83D3D] hover:text-white transition-all duration-300 ease-in-out"
+      >
+        <FontAwesomeIcon icon={faLinkedin} size="2xl" title="LinkedIn" />
+      </a>
+      <a
+        href="mailto:georgesdavid.dev@gmail.com"
+        rel="noreferrer"
+        className="text-[#F83D3D] hover:text-white transition-all duration-300 ease-in-out"
+      >
+        <FontAwesomeIcon icon={faEnvelope} size="2xl" title="Gmail" />
+      </a>
+    </footer>
+  );
+};
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,9 @@ export const Header = () => {
     })
 
     return (
-        <header className={scrolled ? 'scrolled' : null}>
+            <header className={scrolled ? 'scrolled' : 
+            
+            'position-sticky top-0 bg-[#212529] text-[#F83D3D] py-4 px-8 border-b-2 border-[#F83D3D] transition-all duration-300 ease-in-out z-50 w-max display-flex justify-end items-center'}>
             <nav>
                 <ul>
                     <li><a href="#tecnologies">Tecnologías</a></li>
